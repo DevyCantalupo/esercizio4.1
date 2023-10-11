f3182835d5cbc36a99b17422e39c5209e1154ee1
Devy Franco Cantalupo <devycantalupo@outlook.com>
2023-10-11 16:05:22 +0200

    First Commit

---
+++ src/eserzicio4di1.java
@@ -0,0 +1,9 @@
+public class eserzicio4di1 {
+    public static int divisione(int a, int b){
+        return a / b;
+    };
+    public static void main(String[] args) {
+        int risultato = divisione(25,2);
+        System.out.println("risultato = " + risultato);
+    }
+}
